86e76a526f53219e5e2ae7858299f0b7fafd671e
Jcgferreira <jcgferreira1220@outlook.com>
2025-04-26 03:22:41 -0300

    Alteração para pagina de calculo

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Calculadora</h1>
+        <p>
+            Realize cálculos de forma simples e rápida, com a calculadora mais
+            simples do mundo.
+        </p>
+    </header>
+</template>
