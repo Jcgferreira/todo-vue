1ddea0ca0c400ad64ab14009c2aa320fc604d4d6
Jcgferreira <jcgferreira1220@outlook.com>
2025-04-29 00:23:56 -0300

    Automação ao inserir os valores para calculo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { reactive } from "vue";
 import Cabecalho from "./components/Cabecalho.vue";
-import Formulario from "./components/Formulario.vue";
 import Resultado from "./components/Resultado.vue";
 
 const estado = reactive({
@@ -43,14 +42,38 @@ const moduloCalculo = () => {
 <template>
   <div class="container">
     <Cabecalho />
-    <Formulario
-      :moduloCalculo="moduloCalculo"
-      :filtraCalculo="filtraCalculo"
-      :estado1="estado.numero1"
-      :estado2="estado.numero2"
-      @atualiza-numero1="atualizaNumero1"
-      @atualiza-numero2="atualizaNumero2"
-    />
+    <form>
+      <div class="col-md-2 mb-3">
+        <select @change="filtraCalculo" class="form-control">
+          <option value="soma">Soma</option>
+          <option value="subtracao">Subtração</option>
+          <option value="multiplicacao">Multiplicação</option>
+          <option value="divisao">Divisão</option>
+        </select>
+      </div>
+      <div class="row">
+        <div class="col">
+          <input
+            :value="estado.numero1"
+            @input="atualizaNumero1($event.target.value)"
+            required
+            type="number"
+            class="form-control"
+            placeholder="Digite um número"
+          />
+        </div>
+        <div class="col">
+          <input
+            :value="estado.numero2"
+            @input="atualizaNumero2($event.target.value)"
+            required
+            type="number"
+            class="form-control"
+            placeholder="Digite outro número"
+          />
+        </div>
+      </div>
+    </form>
     <Resultado :estado="estado.resultado" />
   </div>
 </template>
